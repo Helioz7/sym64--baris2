e77cbbb1344c15b82346f07bbd0adc7f38de5283
Helioz7 <baris.aktas@cf2000.onmicrosoft.com>
2024-10-30 15:49:03 +0100

    db ok

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -5,15 +5,32 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Section;
+use App\Entity\Article;
+use App\Repository\SectionRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
 
 class HomepageController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em,SectionRepository $sections): Response
     {
+        $menu=$em->getRepository(Section::class)->findAll();
         return $this->render('homepage/index.html.twig', [
-            'controller_name' => 'HomepageController',
+            'menu' => $menu,
+            'sections' => $sections->findAll(),
+        ]);
+    }
+
+
+    #[Route('section/{slug}', name: 'app_section')]
+    public function section(EntityManagerInterface $em,SectionRepository $sections,string $slug): Response
+    {
+        $menu=$em->getRepository(Section::class)->findAll();
+        return $this->render('homepage/index.html.twig', [
+            'menu' => $menu,
+            'sections' => $sections->findAll(),
         ]);
     }
 }
-
