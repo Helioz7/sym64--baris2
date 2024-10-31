424ae25cf792ff5ffeb782e37d7f42b7de7917b1
Helioz7 <baris.aktas@cf2000.onmicrosoft.com>
2024-10-31 12:09:19 +0100

    db fix

---
+++ src/Controller/ArticleDetailController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ArticleDetailController extends AbstractController
+{
+    #[Route('/article/detail', name: 'app_article_detail')]
+    public function index(): Response
+    {
+        return $this->render('article_detail/index.html.twig', [
+            'controller_name' => 'ArticleDetailController',
+        ]);
+    }
+}
--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -8,31 +8,44 @@ use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\Section;
 use App\Entity\Article;
 use App\Repository\SectionRepository;
+use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\Mapping\Entity;
 
 class HomepageController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function index(EntityManagerInterface $em,SectionRepository $sections): Response
-    {
-        $menu=$em->getRepository(Section::class)->findAll();
-        $articles = $em->getRepository(Article::class)->findAll();
+    public function index(
+        EntityManagerInterface $em,
+        SectionRepository $sections,
+        ArticleRepository $articles
+    ): Response {
         return $this->render('homepage/index.html.twig', [
-            'menu' => $menu,
+            'menu' => $sections->findAll(),
             'sections' => $sections->findAll(),
-            'articles' => $articles,
+            'articles' => $articles->findBy([], null, 10),
         ]);
     }
 
+    #[Route('/section/{slug}', name: 'app_section')]
+    public function section(
+        SectionRepository $sections,
+        ArticleRepository $articles,
+        string $slug
+    ): Response {
+        // Récupération de la section spécifique
+        $section = $sections->findOneBy(["sectionSlug" => $slug]);
 
-    #[Route('section/{slug}', name: 'app_section')]
-    public function section(EntityManagerInterface $em,SectionRepository $sections,string $slug): Response
-    {
-        $menu=$em->getRepository(Section::class)->findAll();
-        return $this->render('homepage/index.html.twig', [
-            'menu' => $menu,
-            'sections' => $sections->findAll(),
+        
+        if (!$section) {
+            throw $this->createNotFoundException('La section demandée n\'existe pas');
+        }
+
+        return $this->render('homepage/section.html.twig', [
+            'title' => 'Section ' . $section->getSectionTitle(),
+            'sectionDetail' => $section->getSectionDetail(),
+            'section' => $section,
+            'sections' => $sections->findAll(), 
+            'articles' => $articles->findBy([], null, 10),
         ]);
     }
 }
